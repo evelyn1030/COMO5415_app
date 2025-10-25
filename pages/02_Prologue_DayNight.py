f3e6225679c04d1732bc99155b14e321efcb65b7
evelynsun <evelynsun98@gmail.com>
2025-10-26 01:26:43 +1100

    update1025/21

--- pages/02_Prologue_DayNight.py
+++ pages/02_Prologue_DayNight.py
@@ -97,7 +97,7 @@ with night_tab:
     with colR:
         st.subheader("Chinese People: A Century in Motion (1925–2025)")
         st.markdown("""
-    **What you’ll see**  
+    **What you’ll se2**  
     A century in motion — from linen robes to streetwear,  
     from silent portraits to confident stances.  
     Set to a remix of the Yunnan folk song *Mi Du Shan Ge*,  
